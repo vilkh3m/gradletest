af43d47fc78ac29a8904c0ebdcf5f92f0c0d2768
Dominik Pietrzak <dominik@pietrzak.gda.pl>
2022-04-14 19:31:11 +0200

    Init app

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,23 @@
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.concurrent.TimeUnit
+
+/**
+ * Adds a member to this group.
+ * @return the new size of the group.
+ */
+fun main(args: Array<String>) {
+    println("Hello World!")
+    for (i in 1..3) {
+        println(i)
+        TimeUnit.SECONDS.sleep(3L)
+        val current = LocalDateTime.now()
+        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")
+        val formatted = current.format(formatter)
+        println("Current Date and Time is: $formatted")
+    }
+
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("Program arguments: ${args.joinToString()}")
+}
